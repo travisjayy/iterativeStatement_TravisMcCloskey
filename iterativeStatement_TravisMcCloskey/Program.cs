22f927119825ca7265d600c2ebeed2980182ee04
Travis McCloskey <tmccloskey@usf.edu>
2020-02-19 21:42:12 -0500

    edit

--- iterativeStatement_TravisMcCloskey/Program.cs
+++ iterativeStatement_TravisMcCloskey/Program.cs
@@ -6,7 +6,30 @@ namespace iterativeStatement_TravisMcCloskey
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Input a number between 2 and 10 and witness some magic:");
+            int start = 0;
+            try
+            {
+                string input = Console.ReadLine();
+                int inputValue = int.Parse(input);
+                if (inputValue % 2 == start)
+                {
+                    for (int userInput = start; userInput < inputValue + 1; userInput++)
+                    {
+                        Console.WriteLine(userInput.ToString() + "." + "You entered an even number :D");
+                    }
+                }
+                else
+                {
+                    for (int userInput = start; userInput < inputValue - 1; userInput++)
+                    {
+                        Console.WriteLine(userInput.ToString() + "." + "You entered an odd number D;");
+                    }
+                }
+            }
+            catch
+            {
+            }
         }
     }
 }
